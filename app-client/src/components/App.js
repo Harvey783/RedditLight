9f485378d8cd97f573e17f1118f96588ff0398ef
Chris <chharvey@icloud.com>
2018-12-17 14:16:09 -0500

    initial App BrowserRouter navigation

--- app-client/src/components/App.js
+++ app-client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import PostCreate from './posts/PostCreate';
+import PostEdit from './posts/PostEdit';
+import PostDelete from './posts/PostDelete';
+import PostList from './posts/PostList';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PostList} />
+          <Route path="/posts/new" exact component={PostCreate} />
+          <Route path="/posts/edit" exact component={PostEdit} />
+          <Route path="/posts/delete" exact component={PostDelete} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
