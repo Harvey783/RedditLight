96d3b28f5a61a69f41d59519265b5c388f7bf89b
Chris <chharvey@icloud.com>
2018-12-17 17:59:44 -0500

    GoogleAuth ComponentDidMount

--- app-client/src/components/GoogleAuth.js
+++ app-client/src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '1058742191222-g6utvrli9dt01n71d2elocr2u4aq2usv.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
   render() {
     return <div>GoogleAuth</div>;
   }
