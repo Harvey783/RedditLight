37fb733e7e02afb13b1bbdf880818464a3d5f2db
Chris <chharvey@icloud.com>
2018-12-17 17:44:09 -0500

    initial GoogleAuth component

--- app-client/src/components/Header.js
+++ app-client/src/components/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return (
     <div className="ui secondary menu">
       <Link to="/" className="item">
-        PostIT
+        All Posts
       </Link>
       <div className="right menu">
-        <Link to="/" className="item">
-          All Posts
-        </Link>
+        <GoogleAuth />
       </div>
     </div>
   );
