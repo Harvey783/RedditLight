4b653c59279f634b18fded3764e2a5b5b0b6bcfe
Christopher Harvey <chharvey@icloud.com>
2018-12-17 08:52:09 -0500

    Post Create

--- app-api/app/controllers/api/posts_controller.rb
+++ app-api/app/controllers/api/posts_controller.rb
@@ -3,4 +3,13 @@ class Api::PostsController < ApplicationController
     def index
         render json: Post.all
     end
+
+    def create
+        post = Post.new(post_params)
+        if post.save
+            render json: post
+        else
+            render json: { message: post.errors }, status: 400
+        end
+    end
 end
