5247d01663098b7528801c15b2b5c7d162e315a3
Chris <chharvey@icloud.com>
2018-12-17 17:14:29 -0500

    initial Header component

--- app-client/src/components/App.js
+++ app-client/src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import PostCreate from './posts/PostCreate';
 import PostEdit from './posts/PostEdit';
 import PostDelete from './posts/PostDelete';
 import PostList from './posts/PostList';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={PostList} />
           <Route path="/posts/new" exact component={PostCreate} />
           <Route path="/posts/edit" exact component={PostEdit} />
