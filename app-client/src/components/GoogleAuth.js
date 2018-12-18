44e7dc33b16130c3b99323dfea25bbc745101549
Chris <chharvey@icloud.com>
2018-12-17 20:23:37 -0500

    GoogleAuth renderAuthButton onClick

--- app-client/src/components/GoogleAuth.js
+++ app-client/src/components/GoogleAuth.js
@@ -23,19 +23,33 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="mini ui grey google button">
+        <button
+          onClick={this.onSignOutClick}
+          className="mini ui grey google button"
+        >
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className=" mini ui red google button">
+        <button
+          onClick={this.onSignInClick}
+          className=" mini ui red google button"
+        >
           <i className="google icon" />
           Sign in with Google
         </button>
