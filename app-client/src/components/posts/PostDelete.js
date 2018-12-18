68eac6ef4d65e421f1b33e0f1c6bfb7919502649
Chris <chharvey@icloud.com>
2018-12-18 09:44:46 -0500

    PostDelete Modal

--- app-client/src/components/posts/PostDelete.js
+++ app-client/src/components/posts/PostDelete.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../../actions';
 
 class PostDelete extends React.Component {
   render() {
@@ -6,4 +8,11 @@ class PostDelete extends React.Component {
   }
 }
 
-export default PostDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { post: state.posts[ownProps.match.params.id] };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchPost, deletePost }
+)(PostDelete);
