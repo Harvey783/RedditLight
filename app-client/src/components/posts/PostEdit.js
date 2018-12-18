fe5f4a6354c1f36e7d087fc09e6bff0b02c699cd
Chris <chharvey@icloud.com>
2018-12-18 08:53:28 -0500

    PostEdit mapStateToProps

--- app-client/src/components/posts/PostEdit.js
+++ app-client/src/components/posts/PostEdit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class PostEdit extends React.Component {
   render() {
@@ -6,4 +7,8 @@ class PostEdit extends React.Component {
   }
 }
 
-export default PostEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { post: state.posts[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps)(PostEdit);
