d79711692f12c5798775e7a4c6d7acfc4792df91
Chris <chharvey@icloud.com>
2018-12-18 07:47:20 -0500

    lodash & PostList

--- app-client/src/reducers/index.js
+++ app-client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import { reducer as formReducer } from 'redux-form';
+import postReducer from './postReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer
+  form: formReducer,
+  posts: postReducer
 });
--- app-client/src/reducers/postReducer.js
+++ app-client/src/reducers/postReducer.js
@@ -5,11 +5,20 @@ import {
   EDIT_POST,
   DELETE_POST
 } from '../actions/types';
+import _ from 'lodash';
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_POSTS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
+    case FETCH_POST:
+      return { ...state, [action.payload.id]: action.payload };
     case CREATE_POST:
       return { ...state, [action.payload.id]: action.payload };
+    case EDIT_POST:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
