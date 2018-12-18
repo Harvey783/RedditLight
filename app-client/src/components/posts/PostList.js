3fb2c6f0e6e2f99dd085b18d12d518d232e3d751
Chris <chharvey@icloud.com>
2018-12-18 07:54:49 -0500

    renderIconLinks

--- app-client/src/components/posts/PostList.js
+++ app-client/src/components/posts/PostList.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../../actions';
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
+  }
+
+  renderIconLinks(post) {
+    if (post.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to={`/posts/edit/${post.id}`}>
+            <i class="grey edit outline icon" />
+          </Link>
+          <Link to={`/posts/delete/${post.id}`}>
+            <i class="red x icon" />
+          </Link>
+        </div>
+      );
+    }
   }
 
   render() {
