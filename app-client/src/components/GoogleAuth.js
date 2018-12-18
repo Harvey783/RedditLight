4d5f96fc16248e7953535cbe53b0d56055ad3b6f
Chris <chharvey@icloud.com>
2018-12-17 20:06:15 -0500

    GoogleAuth componentDidMount & renderAuthButton

--- app-client/src/components/GoogleAuth.js
+++ app-client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '1058742191222-g6utvrli9dt01n71d2elocr2u4aq2usv.apps.googleusercontent.com',
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '1058742191222-g6utvrli9dt01n71d2elocr2u4aq2usv.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
