14bb2f5e8830c516a3089f2a8c3c70ffd8f05da2
Chris <chharvey@icloud.com>
2018-12-18 01:09:27 -0500

    PostCreate Form

--- app-client/src/components/posts/PostCreate.js
+++ app-client/src/components/posts/PostCreate.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostCreate extends React.Component {
+  renderInput(formProps) {
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
+
   render() {
-    return <div>PostCreate</div>;
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
   }
 }
 
-export default PostCreate;
+export default reduxForm({
+  form: 'postCreate'
+})(PostCreate);
