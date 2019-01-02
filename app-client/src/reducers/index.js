a2780bc1f8267d65a2cc9b5b452d80d7be46e5fb
Chris <chharvey@icloud.com>
2019-01-01 20:16:19 -0500

    comment edit

--- app-client/src/reducers/index.js
+++ app-client/src/reducers/index.js
@@ -1,4 +1,4 @@
-// The combineReducers helper function turns an object whose values are different reducing functions into a single reducing function you can pass to createStore. The resulting reducer calls every child reducer, and gathers their results into a single state object. The state produced by combineReducers() namespaces the states of each reducer under their keys as passed to combineReducers()
+// The combineReducers helper function turns an object whose values are different reducing functions into a single reducing function that you can pass to createStore. The resulting reducer calls every child reducer, and gathers their results into a single state object.
 import postReducer from './postReducer';
 import authReducer from './authReducer';
 import { combineReducers } from 'redux';
