2d5b62ce75a1c6b7b122dcc90c28cf77296ec333
Christopher Harvey <chharvey@icloud.com>
2019-01-03 15:28:54 -0500

    Modal comments

--- app-client/src/components/Modal.js
+++ app-client/src/components/Modal.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Modal = props => {
+  // defined functional component to receive some number of props
   return ReactDOM.createPortal(
+    // A portal allows us to render some element not as a direct child. We can instead render that element or that component as a child of some other element inside our HTML structure... To create a portal we return a reference or a call to react on Create portal. The first argument is going to be some JSX. The second argument is going to be a reference to a HTML element... PostDelete renders modal. Modal inserted as child to that div with the ID of modal in public/index.html
     <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={e => e.stopPropagation()}
+        // whenever a event occurs an event argument or event object is the argument. And on that event argument we can call e.stopPropagation to make sure that that event does not continue to bubble up causing the window to get dismissed by navigating away
         className="ui standard modal visible active"
       >
         <div className="header" />
@@ -13,7 +16,7 @@ const Modal = props => {
         <div className="actions">{props.actions}</div>
       </div>
     </div>,
-    document.querySelector('#modal')
+    document.querySelector("#modal")
   );
 };
 
