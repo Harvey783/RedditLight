9a2e29d2f5bf410ef5d47f544c49535cb9b0d9d0
Christopher Harvey <chharvey@icloud.com>
2019-01-03 15:12:10 -0500

    action type.js comments

--- app-client/src/actions/types.js
+++ app-client/src/actions/types.js
@@ -1,7 +1,8 @@
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const FETCH_POST = 'FETCH_POST';
-export const CREATE_POST = 'CREATE_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const EDIT_POST = 'EDIT_POST';
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+export const FETCH_POSTS = "FETCH_POSTS";
+export const FETCH_POST = "FETCH_POST";
+export const CREATE_POST = "CREATE_POST";
+export const DELETE_POST = "DELETE_POST";
+export const EDIT_POST = "EDIT_POST";
+// Defines all of the strings that can be used inside the action creators and reducers. Still uses strings to coordinate types but those strings are being assigned to variables. Simply easier to find errors pertaining to variables.
