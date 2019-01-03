20b0e745c7535f69a098f59f70c14a7ba1bea111
Christopher Harvey <chharvey@icloud.com>
2019-01-03 15:32:18 -0500

    authReducer comments

--- app-client/src/reducers/authReducer.js
+++ app-client/src/reducers/authReducer.js
@@ -5,10 +5,12 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SIGN_IN':
+    case "SIGN_IN":
       return { ...state, isSignedIn: true, userId: action.payload };
-    case 'SIGN_OUT':
+    // Modify a property with the spread syntax... Create a new object. Take all the values out of the state argument and into that new object... Updates the isSignedIn property to true... Updates the userId property with a new value from action.payload.
+    case "SIGN_OUT":
       return { ...state, isSignedIn: false, userId: null };
+    // Updates userId to null upon signout
     default:
       return state;
   }
