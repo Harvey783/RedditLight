166cf0b415ed151617a42c50f36ba632805c355b
Christopher Harvey <chharvey@icloud.com>
2019-01-03 15:15:26 -0500

    PostDelete comments

--- app-client/src/components/posts/PostDelete.js
+++ app-client/src/components/posts/PostDelete.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Modal from '../Modal';
-import history from '../History';
-import { connect } from 'react-redux';
-import { fetchPost, deletePost } from '../../actions';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Modal from "../Modal";
+import history from "../History";
+import { connect } from "react-redux";
+import { fetchPost, deletePost } from "../../actions";
+import { Link } from "react-router-dom";
 
 class PostDelete extends React.Component {
   componentDidMount() {
@@ -14,9 +14,11 @@ class PostDelete extends React.Component {
     const { id } = this.props.match.params;
 
     return (
+      // Trying to place some sibling elements next to each other but wrapping them in a single outside <div> tag throws off styling... So React Fragments. Fragments return multiple elements or assigned multiple elements to a single variable, and are rendered without any change to the HTML
       <React.Fragment>
         <button
           onClick={() => this.props.deletePost(id)}
+          // An arrow function is passed into the onClick hadler. An arrow function is used to call deletePost with a particular ID b/c passing a reference to a function off to onClick allows the ID of the deleted post to be passed as well.
           className="ui button negative"
         >
           Delete
@@ -29,8 +31,9 @@ class PostDelete extends React.Component {
   }
 
   renderContent() {
+    // Returns a placeholder when no post is present. Otherwise it displays the title of the post
     if (!this.props.post) {
-      return 'Are you sure you want to delete this post?';
+      return "Are you sure you want to delete this post?";
     }
     return `Delete ${this.props.post.title}???`;
   }
@@ -40,7 +43,8 @@ class PostDelete extends React.Component {
       <Modal
         content={this.renderContent()}
         actions={this.renderActions()}
-        onDismiss={() => history.push('/')}
+        onDismiss={() => history.push("/")}
+        // what happens when a user attempts to click outside and/or dismiss the modal
       />
     );
   }
@@ -48,6 +52,7 @@ class PostDelete extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return { post: state.posts[ownProps.match.params.id] };
+  // Called with both the state from the redux store and a second argument of ownProps. ownProps is the exact same props object that is going to be passed to the component... Use ownPops to look at props.match and pull out the ID of the required post... Returns an object with the key of post. State.posts is an object containing all the different posts where all the keys are the IDs of each post.
 };
 
 export default connect(
