53466a453496ed0b44edf0145d586af42688b0bd
Christopher Harvey <chharvey@icloud.com>
2019-01-13 01:52:16 -0500

    authReducer case change

--- app-client/src/reducers/authReducer.js
+++ app-client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 const INITIAL_STATE = {
   isSignedIn: null,
   userId: null
@@ -5,13 +7,13 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true, userId: action.payload };
     // Modify a property with the spread syntax... Create a new object.
     // Take all the values out of the state argument into that new object
     // Updates the isSignedIn property to true
     // Updates the userId property with a new value from action.payload.
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false, userId: null };
     // Updates userId to null upon signout
     default:
