81e73263357f780c6db46b5f38dbb43f99847219
Christopher Harvey <chharvey@icloud>
2019-04-07 08:22:00 -0400

    changes

--- app-client/src/components/App.js
+++ app-client/src/components/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import PostCreate from "./posts/PostCreate";
-import PostEdit from "./posts/PostEdit";
-import PostDelete from "./posts/PostDelete";
-import PostList from "./posts/PostList";
-import { fetchPosts } from "../actions/index";
-import Header from "./Header";
-import history from "./History";
-import { connect } from "react-redux";
-import { Router, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import PostCreate from './posts/PostCreate';
+import PostEdit from './posts/PostEdit';
+import PostDelete from './posts/PostDelete';
+import PostList from './posts/PostList';
+import { fetchPosts } from '../actions/index';
+import Header from './Header';
+import history from './History';
+import { connect } from 'react-redux';
+import { Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   componentDidMount() {
